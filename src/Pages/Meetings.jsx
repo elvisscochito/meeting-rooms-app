557b58800fb39eada809d6e8d591e11afa106451
elvisscochito <elvirodominguezsoriano@outlook.com>
2023-08-07 12:21:04 -0600

    fix: :recycle: Meetings.jsx was updated
    Test data was combined into rooms with meetings inside it

--- src/Pages/Meetings.jsx
+++ src/Pages/Meetings.jsx
@@ -6,46 +6,44 @@ function Meetings() {
   const [rooms, setRooms] = useState([
     {
       id: 1,
-      name: "P101"
+      name: "P101",
+      meetings: [
+        {
+          id: 1,
+          title: "Today's work",
+          description: "Let's talk about today's work",
+          date: "August 1st, 2021",
+          time: "10am - 12pm (2hrs)",
+          host: "Apaez",
+          active: true
+        },
+        {
+          id: 2,
+          title: "Client's needs",
+          description: "What client are looking for?",
+          date: "August 2nd, 2021",
+          time: "1pm - 3pm (2hrs)",
+          host: "Apaez",
+          active: false
+        },
+        {
+          id: 3,
+          title: "Review today's work",
+          description: "Review today's work",
+          date: "August 3rd, 2021",
+          time: "4pm - 6pm (2hrs)",
+          host: "Apaez",
+          active: false
+        },
+      ]
     },
     {
       id: 2,
-      name: "P102"
+      name: "P102",
+      meetings: []
     }
   ]);
   const [currentActiveTab, setCurrentActiveTab] = useState(rooms[0].name);
-  const [meetings, setMeetings] = useState([
-    {
-      id: 1,
-      title: "Today's work",
-      description: "Let's talk about today's work",
-      date: "August 1st, 2021",
-      time: "10am - 12pm (2hrs)",
-      host: "Apaez",
-      active: true,
-      room: "P101"
-    },
-    {
-      id: 2,
-      title: "Client's needs",
-      description: "What client are looking for?",
-      date: "August 2nd, 2021",
-      time: "1pm - 3pm (2hrs)",
-      host: "Apaez",
-      active: false,
-      room: "P101"
-    },
-    {
-      id: 3,
-      title: "Review today's work",
-      description: "Review today's work",
-      date: "August 3rd, 2021",
-      time: "4pm - 6pm (2hrs)",
-      host: "Apaez",
-      active: false,
-      room: "P101"
-    },
-  ]);
   const dialog = useRef(null);
 
   const openModal = () => {
@@ -96,35 +94,37 @@ function Meetings() {
         </div>
 
         <div className={styles.meetings}>
-          {
-            meetings.filter((meeting) => currentActiveTab === meeting.room).length === 0 ? (
-              <span>
-                No meetings upcoming
-                {/*  No hay reuniones programadas para este d&iacute;a */}
-              </span>
-            ) : (
-              meetings.map((meeting) => (
-                currentActiveTab === meeting.room &&
-                <div
-                  className={styles.card}
-                  key={meeting.id}
-                  style={{ borderLeftColor: meeting.active ? "#00743B" : "e6e6e6" }}
-                >
-                  <header>
-                    <h3>{meeting.title}</h3>
-                    <h4>{meeting.description}</h4>
-                  </header>
-                  <footer className={styles.cardFooter}>
-                    <span>
-                      {meeting.time}
-                      {/* 10am - 12pm (2hrs) */}
-                    </span>
-                    <span>{meeting.host}</span>
-                  </footer>
-                </div>
-              )
-              ))}
+          {rooms.map((room) => (
+            <>
+              {room.name === currentActiveTab && (
+                room.meetings.length === 0 ? (
+                  <span>
+                    No meetings upcoming
+                    {/* Sin reuniones programadas */}
+                  </span>
+                ) : (
+                  room.meetings.map((meeting) => (
+                    <div
+                      className={styles.card}
+                      key={meeting.id}
+                      style={{ borderLeftColor: meeting.active ? "#00743B" : "#e6e6e6" }}
+                    >
+                      <header>
+                        <h3>{meeting.title}</h3>
+                        <h4>{meeting.description}</h4>
+                      </header>
+                      <footer className={styles.cardFooter}>
+                        <span>{meeting.time}</span>
+                        <span>{meeting.host}</span>
+                      </footer>
+                    </div>
+                  ))
+                )
+              )}
+            </>
+          ))}
         </div>
+
       </div>
       <footer className={styles.footer}>
         <dialog ref={dialog}>
