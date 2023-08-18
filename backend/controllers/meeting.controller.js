a0b96602adee011e444bc30a1c2b8860436a9f09
elvisscochito <elvirodominguezsoriano@outlook.com>
2023-08-18 12:10:50 -0600

    feat: :sparkles: Date validations in API
    Check if meeting schedule times are avalible before create meeting

--- backend/controllers/meeting.controller.js
+++ backend/controllers/meeting.controller.js
@@ -27,6 +27,29 @@ export const postMeeting = async (req, res) => {
       return res.status(404).json({ message: "Room not found" });
     }
 
+    const start = new Date(req.body.start);
+    const end = new Date(req.body.end);
+
+    /**
+     *  @note check if any meeting overload schedule
+     * if any of these conditions are true, the document will be considered a match
+     * start: meeting starts within the specified range
+     * end: meeting ends within the specified range
+     * meeting spans over the entire specified range
+     */
+    const overlappingMeetings = await meetingModel.find({
+      room: room._id,
+      $or: [
+        { start: { $gte: start, $lt: end } },
+        { end: { $gt: start, $lte: end } },
+        { start: { $lte: start }, end: { $gte: end } }
+      ]
+    });
+
+    if (overlappingMeetings.length > 0) {
+      return res.status(409).json({ message: "Meeting overlaps with another meeting" });
+    }
+
     const newMeeting = await meetingModel.create({
       title: req.body.title,
       description: req.body.description,
