d39b5fa0c5f4bab12c4a4c050ed0d06f158ec424
elvisscochito <elvirodominguezsoriano@outlook.com>
2023-08-24 12:40:25 -0600

    Rename env variable

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async (req, res) => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.name}`);
   } catch (error) {
     res.status(400).json({ error: "Error processing request", details: error.message });
